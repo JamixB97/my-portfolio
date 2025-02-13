98db143d70cc7ec0f31fe7a98cffb78e6030dcbf
JamixB <bamesb@gmail.com>
2025-02-12 20:52:23 -0800

    Created vite, and added Header, Footer, and Nav componants, with pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import Nav from './Nav';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <div className="container">
+      <h1>James Boulden</h1>
+        <Nav />
+    </div>
+  );
+}
---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Resume from './pages/Resume';
+import Portfolio from './pages/Portfolio';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+      {
+        path: '/resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
